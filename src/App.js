3a5d8f8c78565759ff998e5ee5c1a45a04c60b88
Narendra Mandadapu <scriptstar@hotmail.co.uk>
2019-04-23 15:43:36 +0100

    render a react ui with jsx

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +9,22 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to React Redux Todo App</h2>
       </header>
+      <form>
+        <input type="text" />
+      </form>
+      <div className="Todo-List">
+        <ul>
+          <li>
+            <input type="checkbox" /> Create Static UI
+          </li>
+          <li>
+            <input type="checkbox" /> Create Initial State
+          </li>
+          <li>
+            <input type="checkbox" /> Use State to Render UI
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
